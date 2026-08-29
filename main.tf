df767f74025c56880caab5d6041380fff9dc9b9d
Eugene Ermakov <eugeniy696@gmail.com>
2026-08-29 07:02:52 +0300

    solution2

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "resource_group_storage" {
-  source  = "eugeneermakov696/resource_group_storage/azurerm"
-  version = "1.0.0"
-  resource_group_name             = "rg12541"
-  location                        = "West Europe"
-  storage_account_name            = "somestorageacct12541"
+  source               = "eugeneermakov696/resource_group_storage/azurerm"
+  version              = "1.0.0"
+  resource_group_name  = "rg12541"
+  location             = "West Europe"
+  storage_account_name = "somestorageacct12541"
 }
